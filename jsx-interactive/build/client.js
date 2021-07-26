2b67fadcf9eaed015545a82a11c4a49dba5ec2cf
Harsh Gupta <work@hargup.in>
2021-07-26 12:20:15 +0530

    fix webpack config

---
+++ jsx-interactive/build/client.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var React = require('react');
+
+var ReactDOM = require('react-dom');
+
+var Component = require('./Component.js');
+
+ReactDOM.render(React.createElement(Component), document);
--- jsx-interactive/webpack.config.js
+++ jsx-interactive/webpack.config.js
@@ -4,6 +4,6 @@ module.exports = {
   entry: './build/client.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+    path: path.resolve(__dirname, 'public'),
   },
 };
