ba5e00164ce3a5e51fae8e6b0437e9b5c11bd484
Harsh Gupta <work@hargup.in>
2021-07-26 14:03:46 +0530

    clean up the example

--- dynamic-component/server/test_script.js
+++ dynamic-component/server/test_script.js
@@ -7,15 +7,10 @@ var createReactClass = require('create-react-class');
 const TestComponent = createReactClass({
   displayName: "TestComponent",
   render: function render() {
-    return /*#__PURE__*/React.createElement("div", null, "Hello XYZ");
+    return (<div>
+      Hello Dynamic Component
+    </div>);
   }
 });
 
-function hello(x) {
-  return 'Hello ' + x;
-}
-
 window.TestComponent = TestComponent;
-var _default = TestComponent;
-
-exports.default = _default;
