78be581f1c7e7e5df6382d8ac93b34666a21c66c
Harsh Gupta <work@hargup.in>
2021-07-28 15:48:13 +0530

    remove build files

--- jsx-interactive/build/client.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-var React = require('react');
-
-var ReactDOM = require('react-dom');
-
-var Component = require('./Component.js');
-
-ReactDOM.render(React.createElement(Component), document);
