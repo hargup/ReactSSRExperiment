24aa75fbdfce185d3a5eff639fa55e7d94f23828
Harsh Gupta <work@hargup.in>
2021-07-28 15:26:04 +0530

    Convert the JSX example into a single file example

--- minimal-jsx/server/server.js
+++ minimal-jsx/server/server.js
@@ -2,7 +2,17 @@ var express = require('express');
 var app = express();
 var React = require('react');
 var ReactDOMServer = require('react-dom/server');
-var Component = require('./Component.js');
+var createReactClass = require('create-react-class');
+
+var Component = createReactClass({
+  render: function(){
+	return (<div>
+		<h1>Hello World!</h1>
+                <p>Isn't server-side rendering remarkable?</p>
+            </div>);
+	}
+});
+
 
 app.get('/', function(request, response) {
     var html = ReactDOMServer.renderToString(
