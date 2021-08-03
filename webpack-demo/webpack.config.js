a23f5b1a5e8a41e13008b198bd2998a1367ab6b9
Harsh Gupta <work@hargup.in>
2021-08-03 15:01:15 +0530

    Add code from output management chapter

--- webpack-demo/webpack.config.js
+++ webpack-demo/webpack.config.js
@@ -1,17 +1,19 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
+  entry: {
+    index: './src/index.js',
+    print: './src/print.js',
   },
-  module: {
-    rules: [
-      {
-	test: /\.css$/i,
-	use: ['style-loader', 'css-loader'],
-      },
-    ],
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Output Management',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
   },
 };
