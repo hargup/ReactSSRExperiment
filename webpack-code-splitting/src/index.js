f89f99b9e378bc84d7b3e90b26fcf82b8c52ac53
Harsh Gupta <work@hargup.in>
2021-08-03 16:33:35 +0530

    complete the code splitting example

--- webpack-code-splitting/src/index.js
+++ webpack-code-splitting/src/index.js
@@ -1,20 +1,14 @@
-import _ from 'lodash';
-import printMe from './print.js';
 
-function component() {
+async function getComponent() {
     const element = document.createElement('div');
-    const btn = document.createElement('button');
-  
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    const { default: _ } = await import('lodash');
+    const { default: printMe} = await import('./print');
+    element.innerHTML = _.join(['Hello', 'yolo'], ' ');
+    printMe()
 
-    btn.innerHTML = 'Click me and check the console!';
-
-    btn.onclick = printMe;
-
-
-    element.appendChild(btn);
-  
     return element;
   }
   
-  document.body.appendChild(component());
+getComponent().then((component) => {
+  document.body.appendChild(component);
+})
--- webpack-code-splitting/webpack.config.js
+++ webpack-code-splitting/webpack.config.js
@@ -4,15 +4,9 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: {
       index: './src/index.js',
-      another: './src/another-module.js',
     },
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-    optimization: {
-    splitChunks: {
-      chunks: 'all',
-    },
-  },
 };
